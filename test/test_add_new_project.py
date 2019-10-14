bb004fbeb64cd9a51dd59010431edcf7cca77330
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2019-10-14 15:04:48 +0200

    Function that generates random data for test_add_project were added (25).

--- test/test_add_new_project.py
+++ test/test_add_new_project.py
@@ -1,16 +1,27 @@
-from operator import itemgetter
-from collections import OrderedDict
-from string import ascii_uppercase as alphabet
+import pytest
+
 from model.project import Project
+import string
+import random
 
 
-def test_add_new_project(app, json_projects):
-    project = json_projects
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def test_add_new_project(app):
+    project_name = random_string("name", 10)
     app.session.login("administrator", "root")
     old_projects = app.project.get_project_list()
-    app.project.create(project)
-    new_projects = app.project.get_project_list()
-    assert len(old_projects) + 1 == len(new_projects)
-    old_projects.append(project)
-    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
-
+    names_of_existing_projects = []
+    for element in old_projects:
+        name = element.name
+        names_of_existing_projects.append(name)
+    if project_name not in names_of_existing_projects:
+        project = Project(name=project_name)
+        app.project.create(project)
+        new_projects = app.project.get_project_list()
+        assert len(old_projects) + 1 == len(new_projects)
+        old_projects.append(project)
+        assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
